9b41713c1c2e1915be82c6aa41329b94529effa5
gayathri <gayathri@upturntechnology.com>
2022-12-01 17:39:40 +0530

    ooo

---
+++ src/app/user/model/user.model.ts
@@ -0,0 +1,15 @@
+export class User {
+
+    _id?: any;
+    imageUrl?: string;
+    clothName?:string;
+    price?:number;
+    description?: string;
+    category?: string;
+    brand?: string;
+    quantity?: number;
+    maxQuantity?:number;
+    type?: string;
+    discount?: number;
+
+}
